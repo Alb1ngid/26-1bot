7b1f1b7a5bd0c334502c37287c1f70866b624aee
bekkkks <ewefe811@gmail.com>
2023-02-09 19:51:58 +0600

    'lesson2'

--- main.py
+++ main.py
@@ -19,6 +19,20 @@ bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
 
+
+
+
+@db.message_handler(commands=['start', 'hello'])
+async def start_handler(massage: types.Message):
+    await bot.send_message(massage.from_user.id, f'привет {massage.from_user.first_name}')
+    await massage.answer('это ансфер')
+    await massage.reply(massage.from_user.first_name)
+
+@db.message_handler()
+async def echo(massage: types.Message):
+    await bot.send_message(massage.from_user.id, massage.text)
+    await massage.answer('что-то еще?')
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     executor.start_polling(db, skip_updates=True)
