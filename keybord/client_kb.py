c0664992f9212bf89678e382e71d2ab16d74dfbd
bekkkks <ewefe811@gmail.com>
2023-02-27 21:31:51 +0600

    rieusghvr

--- keybord/client_kb.py
+++ keybord/client_kb.py
@@ -8,9 +8,40 @@ start_markup = ReplyKeyboardMarkup(
 start_button = KeyboardButton("/start")
 info_button = KeyboardButton("/info")
 quiz_button = KeyboardButton("/quiz")
+reg_button = KeyboardButton("/reg")
 
 location = KeyboardButton("location", request_location=True)
 contact = KeyboardButton("contact", request_contact=True)
 user = KeyboardButton("user", request_user=None)
 
-start_markup.add(start_button, info_button, quiz_button, location, contact, user)
+start_markup.add(start_button, info_button, quiz_button, location, contact, user, reg_button)
+
+cancel = KeyboardButton('cancel')
+
+submit_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    KeyboardButton('да'),
+    KeyboardButton('миш всё фигня давай по новой'),
+    cancel
+)
+
+gender_murkup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    KeyboardButton('мужчина'),
+    KeyboardButton('женчина'),
+    KeyboardButton('ленолиум'),
+    KeyboardButton('ломинат'),
+    KeyboardButton('индивиды'),
+    cancel
+)
+
+cancel_markup = ReplyKeyboardMarkup(
+    resize_keyboard=True,
+    one_time_keyboard=True
+).add(
+    cancel
+)
